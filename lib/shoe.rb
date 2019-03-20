6e635102f64c7efb9a8cfd72742c65251867d005
Eric Storey <estorey@gmail.com>
2019-03-20 01:47:56 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,2 +1,12 @@
 # Make your shoe class here!
 
+class Shoe 
+  attr_accessor :brand, :color, :size, :material, :condition
+  
+  def initialize(brand)
+    @brand=brand
+  end 
+  
+  def cobble
+  
+end
